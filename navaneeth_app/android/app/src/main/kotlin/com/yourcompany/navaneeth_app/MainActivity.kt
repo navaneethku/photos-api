c5399c154d7dd2c7a34011fdfaec509654fc55d4
navaneethku <navaneethku1729@gmail.com>
2023-03-02 14:58:17 +0530

    initial commit

---
+++ navaneeth_app/android/app/src/main/kotlin/com/yourcompany/navaneeth_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.yourcompany.navaneeth_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ navaneeth_app/lib/app/routes/app_pages.dart
@@ -0,0 +1,27 @@
+import 'package:get/get.dart';
+
+import 'package:navaneeth_app/app/modules/Register/bindings/register_binding.dart';
+import 'package:navaneeth_app/app/modules/Register/views/register_view.dart';
+import 'package:navaneeth_app/app/modules/home/bindings/home_binding.dart';
+import 'package:navaneeth_app/app/modules/home/views/home_view.dart';
+
+part 'app_routes.dart';
+
+class AppPages {
+  AppPages._();
+
+  static const INITIAL = Routes.REGISTER;
+
+  static final routes = [
+    GetPage(
+      name: _Paths.HOME,
+      page: () => HomeView(),
+      binding: HomeBinding(),
+    ),
+    GetPage(
+      name: _Paths.REGISTER,
+      page: () => RegisterView(),
+      binding: RegisterBinding(),
+    ),
+  ];
+}
